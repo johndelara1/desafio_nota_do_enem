4b3c445ba499f67e2bef8d9f089ce71655b08095
John de Lara <johndelara1@gmail.com>
2019-04-26 20:53:57 -0300

    nota 93.26 no desafio

--- PrevendoNotaDeMatematica.r
+++ PrevendoNotaDeMatematica.r
@@ -5,12 +5,13 @@
 setwd("C:/Users/Matilde/Dropbox/DESAFIO-Codenation(ENEM)/desafio_nota_do_enem")
 getwd()
 
-library(dplyr) # Filtragens
-#library(tidyr) 
-library(psych) # Scatterplot Matrix
-library(e1071) # SVM
-library(rpart) # RANDON FOREST
-library(readr) # GERAR CSV
+library(dplyr)     # Filtragens
+library(psych)     # Scatterplot Matrix
+library(e1071)     # SVM
+library(rpart)     # RANDON FOREST
+library(readr)     # GERAR CSV
+library(tidyverse) # Drop_NA()
+library(readxl)    # Importar xls
 
 # ****************************************************
 # ***                   DADOS                      ***
@@ -19,21 +20,74 @@ library(readr) # GERAR CSV
 # Etapa 1 - Coletando os dados
 nota <- read.csv("train.csv")
 notateste <- read.csv("test.csv")
+PIB_2016 <- read_excel("PIB-2016.xls")
+
+
+# ****************************************************
+# ***                   INCLUIR                    ***
+# ***                NOVAS VARIÁVEIS               ***
+# ****************************************************
+#média do pib por estado
+PIB_2016 = PIB_2016 %>% filter(Ano == 2016)
+PIB_2016 = PIB_2016[c(names(PIB_2016)[5], names(PIB_2016)[42])]
+MEDIA_PIB_2016 = aggregate(PIB_2016$`Produto Interno Bruto per capita
+(R$ 1,00)`, 
+          by=list(PIB_2016$`Sigla da Unidade da Federação`),
+          FUN = mean)
+colnames(MEDIA_PIB_2016) = c("SG_UF_RESIDENCIA","media")
+
+# Criar regioes com os estados por prova
+glimpse(notateste$SG_UF_RESIDENCIA)
+table(notateste$SG_UF_RESIDENCIA)
+x <- c(notateste$SG_UF_RESIDENCIA)
+lookup <- c( AC = "NORTE",  AL = "NORDESTE",  AM = "NORTE",  AP = "NORTE",  BA = "NORDESTE", CE = "NORDESTE", DF = "CENTRO-OESTE", ES = "SUDESTE", GO = "CENTRO-OESTE", MA = "NORDESTE", MG = "SUDESTE", MS = "CENTRO-OESTE", MT = "CENTRO-OESTE", PA = "NORTE", PB = "NORDESTE", PE = "NORDESTE", PI = "NORDESTE", PR = "SUL", RJ = "SUDESTE", RN = "NORDESTE", RO = "NORTE", RR = "NORTE", RS = "SUL", SC = "SUL", SE = "NORDESTE", SP = "SUDESTE", TO = "NORTE")
+uniao = lookup[x]
+regioes = unname(uniao)
+table(regioes)
+notateste$regioes = regioes
+table(notateste$regioes)
+names(table(notateste$regioes))
+
+#Transformar os dois datasets com as mesmas variáveis
+notateste = merge(notateste,MEDIA_PIB_2016)
 nomes <- names(notateste)
+
+
+
+# Criar regioes com os estados por prova
+glimpse(nota$SG_UF_RESIDENCIA)
+table(nota$SG_UF_RESIDENCIA)
+x <- c(nota$SG_UF_RESIDENCIA)
+lookup <- c( AC = "NORTE",  AL = "NORDESTE",  AM = "NORTE",  AP = "NORTE",  BA = "NORDESTE", CE = "NORDESTE", DF = "CENTRO-OESTE", ES = "SUDESTE", GO = "CENTRO-OESTE", MA = "NORDESTE", MG = "SUDESTE", MS = "CENTRO-OESTE", MT = "CENTRO-OESTE", PA = "NORTE", PB = "NORDESTE", PE = "NORDESTE", PI = "NORDESTE", PR = "SUL", RJ = "SUDESTE", RN = "NORDESTE", RO = "NORTE", RR = "NORTE", RS = "SUL", SC = "SUL", SE = "NORDESTE", SP = "SUDESTE", TO = "NORTE")
+uniao = lookup[x]
+regioes = unname(uniao)
+table(regioes)
+nota$regioes = regioes
+table(nota$regioes)
+names(table(nota$regioes))
+
+#Transformar os dois datasets com as mesmas variáveis
+nota = merge(nota,MEDIA_PIB_2016)
+
 
 # ****************************************************
 # ***                   TREINO                     ***
 # ***                                              ***
 # ****************************************************
 # Segmentando apenas os dados que temos no teste
-nota = nota[c(nomes, "NU_NOTA_MT")]
-treinando = nota %>% drop_na()
-treinando$Q027 = NULL
+treinando = nota[c(nomes, "NU_NOTA_MT")]
+treinando$Q027 = as.numeric(treinando$Q027)
+treinando$Q027[(treinando$Q027 == 1)] <- mean(treinando$Q027)
+glimpse(treinando)
+
+#Para que eu possa pegar exatamente os nomes das variáveis mais a target
+treinando = treinando %>% drop_na()
+#treinando$Q027 = NULL
 treinando$TP_DEPENDENCIA_ADM_ESC = NULL
 treinando$TP_ENSINO = NULL
 any(is.na(treinando))
 # ****************************************************
-# ***                Padronizados                  ***
+# ***              FORMATO NUMÉRICO                ***
 # ****************************************************
 glimpse(treinando)
 #Transformar para numérico
@@ -50,6 +104,9 @@ treinando$CO_PROVA_CN = as.numeric(treinando$CO_PROVA_CN)
 treinando$CO_PROVA_CH = as.numeric(treinando$CO_PROVA_CH)
 treinando$CO_PROVA_LC = as.numeric(treinando$CO_PROVA_LC)
 treinando$CO_PROVA_MT = as.numeric(treinando$CO_PROVA_MT)
+treinando$regioes = as.factor(treinando$regioes)
+treinando$regioes = as.numeric(treinando$regioes)
+treinando = treinando[c(-1)]
 nota = treinando
 treinando = treinando[c(-1)]
 
@@ -63,7 +120,10 @@ glimpse(treinando)
 
 # Criar variável de segurança
 testando = notateste
-testando$Q027 = NULL
+testando$Q027 = as.numeric(testando$Q027)
+testando$Q027[(treinando$Q027 == 1)] <- mean(testando$Q027)
+
+#testando$Q027 = NULL
 testando$TP_DEPENDENCIA_ADM_ESC = NULL
 testando$TP_ENSINO = NULL
 
@@ -85,6 +145,9 @@ testando$CO_PROVA_CN = as.numeric(testando$CO_PROVA_CN)
 testando$CO_PROVA_CH = as.numeric(testando$CO_PROVA_CH)
 testando$CO_PROVA_LC = as.numeric(testando$CO_PROVA_LC)
 testando$CO_PROVA_MT = as.numeric(testando$CO_PROVA_MT)
+testando$regioes = as.factor(testando$regioes)
+testando$regioes = as.numeric(testando$regioes)
+testando = testando[c(-1)]
 notateste = testando
 testando = testando[c(-1)]
 
@@ -97,7 +160,7 @@ glimpse(testando)
 # ***                                              ***
 # ****************************************************
 
-##Regressão linear multipla
+##Regressão linear múltipla
 # Etapa 3: Treinando o Modelo (usando os dados de treino)
 modelo <- lm(NU_NOTA_MT ~ ., data = treinando)
 
@@ -106,6 +169,7 @@ modelo
 
 previsao1 <- predict(modelo)
 View(previsao1)
+
 #treinando$Prev = previsao1
 
 # Etapa 4: Avaliando a Performance do Modelo
@@ -122,8 +186,9 @@ trainset = treinando
 testset = testando
 
 
-modelo_rf_v1 = rpart(NU_NOTA_MT ~ ., data = trainset, control = rpart.control(cp = .0005)) 
+modelo_rf_v1 = rpart(NU_NOTA_MT ~ ., data = trainset, control = rpart.control( cp = .0006)) 
 summary(modelo_rf_v1)
+
 # Previsões nos dados de teste
 tree_pred = predict(modelo_rf_v1, testset)
 
@@ -131,3 +196,4 @@ enviotree = notateste[1]
 enviotree$NU_NOTA_MT = tree_pred
 
 write.csv(enviotree, "answer.csv", row.names = FALSE)
+
